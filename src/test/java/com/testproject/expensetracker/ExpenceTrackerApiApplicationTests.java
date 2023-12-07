0545f635260abba22471f116582a47330df5f4e3
PedroSagredo <pedro.sagredof@gmail.com>
2023-12-07 18:43:58 -0300

    First Commit - Create empty project

---
+++ src/test/java/com/testproject/expensetracker/ExpenceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
