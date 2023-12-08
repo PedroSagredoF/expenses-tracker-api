40b6b37922ae135a319af22a546c61fce66830e0
PedroSagredo <pedro.sagredof@gmail.com>
2023-12-07 23:29:17 -0300

    Create UserRepository interface

---
+++ src/main/java/com/testproject/expensetracker/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.testproject.expensetracker.repositories;
+
+import com.testproject.expensetracker.domain.User;
+import com.testproject.expensetracker.exceptions.EtAuthException;
+
+public interface UserRepository {
+
+    Integer createUser(String firstName, String lastName, String email, String password) throws EtAuthException;
+    User findEmailAndPass(String email, String pass) throws EtAuthException;
+    Integer getCountByEmail(String email);
+    User findUserById(Integer userId);
+}
