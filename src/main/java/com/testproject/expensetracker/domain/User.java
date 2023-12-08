edd3e2e891f36962fa74af45cb922a5ef7a1ba7b
PedroSagredo <pedro.sagredof@gmail.com>
2023-12-07 22:53:54 -0300

    Adding getter, setter and constructor to User class

--- src/main/java/com/testproject/expensetracker/domain/User.java
+++ src/main/java/com/testproject/expensetracker/domain/User.java
@@ -5,5 +5,52 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
-    private String
+    private String password;
+
+    public User(Integer user_id, String firstName, String lastName, String email, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
