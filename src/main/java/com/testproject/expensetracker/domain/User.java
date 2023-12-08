4acaf48769d0719f9ad3cde36de0eaba846e18cb
PedroSagredo <pedro.sagredof@gmail.com>
2023-12-07 22:48:27 -0300

    Create domain package and User class

---
+++ src/main/java/com/testproject/expensetracker/domain/User.java
@@ -0,0 +1,9 @@
+package com.testproject.expensetracker.domain;
+
+public class User {
+    private Integer user_id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String
+}
