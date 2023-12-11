a1e58c020d4cfdc2b65d38e7b6a43b7fea02489d
PedroSagredo <pedro.sagredof@gmail.com>
2023-12-11 12:12:38 -0300

    createUser function update

--- src/main/java/com/testproject/expensetracker/repositories/UserRepositoryImpl.java
+++ src/main/java/com/testproject/expensetracker/repositories/UserRepositoryImpl.java
@@ -2,16 +2,41 @@ package com.testproject.expensetracker.repositories;
 
 import com.testproject.expensetracker.domain.User;
 import com.testproject.expensetracker.exceptions.EtAuthException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository{
 
     private static final String SQL_CREATE = "INSERT INTO ET_USERS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD) VALUES(NEXTVAL('ET-USERS-SEQ', ?,?,?,?)";
+    
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
-    public Integer createUser(String firstName, String lastName, String email, String password) throws EtAuthException {
-        return null;
+    public Integer createUser(String firstName, String lastName, String email, String password)
+            throws EtAuthException {
+        try{
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1,firstName);
+                ps.setString(1,lastName);
+                ps.setString(1,email);
+                ps.setString(1,password);
+                return ps;
+            }, keyHolder);
+            return (Integer) keyHolder.getKeys().get("USER_ID");
+        } catch (Exception e){
+            throw new EtAuthException("Invalid details. Failed to create account");
+        }
     }
 
 
