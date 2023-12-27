673e4a968b7094895f232ad4b3bcb28f48481796
PedroSagredo <pedro.sagredof@gmail.com>
2023-12-27 19:18:38 -0300

    Add lombok dependencie and reformat code.

--- src/main/java/com/testproject/expensetracker/domain/User.java
+++ src/main/java/com/testproject/expensetracker/domain/User.java
@@ -1,5 +1,10 @@
 package com.testproject.expensetracker.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
     private Integer user_id;
     private String firstName;
@@ -7,51 +12,4 @@ public class User {
     private String email;
     private String password;
 
-    public User(Integer user_id, String firstName, String lastName, String email, String password) {
-        this.user_id = user_id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Integer getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Integer user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
